f304c8f9b9ae044f8f655551dffe634307e1de43
aidaramirov <aidaramirov@mail.ru>
2021-05-03 21:56:18 +0300

    Beginning

--- src/app/app-router.ts
+++ src/app/app-router.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { from } from 'rxjs';
 
-import {TodosAddingComponent} from './components/todos-adding/todos-adding.component';
+import {RoomAddComponent} from './components/body/rooms/room-add/room-add.component';
 import {RoomsListComponent} from './components/body/rooms/rooms-list/rooms-list.component';
 import {RoomDetailComponent} from './components/body/rooms/room-detail/room-detail.component';
 
@@ -11,7 +11,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/rooms', pathMatch: 'full' },
   { path: 'rooms', component: RoomsListComponent },
   { path: 'detail/:id', component: RoomDetailComponent },
-  { path: 'add-room', component: TodosAddingComponent}
+  { path: 'add-room', component: RoomAddComponent}
 ];
 
 @NgModule({
